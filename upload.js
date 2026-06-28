b81fe74afd6180199883345dd8a701b247fd49f1
Manik Ahmed <kormoi.com@gmail.com>
2026-06-29 02:21:32 +0600

    readme.md added

--- upload.js
+++ upload.js
@@ -1,11 +1,20 @@
+const path = require("path");
 const fncs = require('./functions');
 const cstyler = require('cstyler');
 const dbtasker = require("dbtasker");
+const fif = require("./filefunctions");
 
 
 
-async function uploadData(config, data, operation) {
+async function uploadBackup(config, zipPath, type = 'merge') {
     try {
+        zipPath = path.resolve(zipPath);
+        const isFile = await fif.isfilepathwithext(zipPath);
+        if (isFile !== true) {
+            throw new Error("A backup.zip file path is required");
+        } else {
+            
+        }
         const dbtaskerdata = data.dbtaskerdata;
         const rows = data.rows;
 
